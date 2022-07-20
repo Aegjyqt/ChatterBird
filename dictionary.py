90c2cf859f374b196f4fcea722c8831087abec93
Василий Перов <vasiliiperov@gmail.com>
2022-07-21 00:21:49 +0700

    added relevance to output message

--- dictionary.py
+++ dictionary.py
@@ -12,15 +12,14 @@ def dict_filler(sheet_name):
     this_dict = {}
     for i in range(2, sheet.max_row):
         temp = list(sheet[i][3].value.lower())
-        for i in temp:
-            if i == '\"':
-                temp.remove(i)
+        for n in temp:
+            if n == '\"':
+                temp.remove(n)
         key = ''.join(temp)
         keys_list.append(key)
-    for i in range(2, sheet.max_row):
         items_list.append(sheet[i][4].value)
-    for item in keys_list:
-        this_dict[item] = items_list[keys_list.index(item)]
+        this_dict[key] = items_list[keys_list.index(key)] + f', актуальность: {sheet[i][2].value}'
+
     big_dict.update(this_dict)
     return this_dict
 
